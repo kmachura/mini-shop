f0a3962351ec817414ac57024b32a5cfc5ec402e
kmachura-mika <katarzyna.machura-mika@opi.org.pl>
2023-10-15 09:53:35 +0200

    adding User entity

---
+++ users/src/main/java/org/ms/minishop/model/RoleEnum.java
@@ -0,0 +1,8 @@
+package org.ms.minishop.model;
+
+public enum RoleEnum {
+
+    USER,
+    ADMIN,
+    WORKER
+}
--- users/src/main/java/org/ms/minishop/model/User.java
+++ users/src/main/java/org/ms/minishop/model/User.java
@@ -1,4 +1,28 @@
 package org.ms.minishop.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+
+    @Id
+    @Column(name = "userId")
+    private UUID id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    @Column(name = "role")
+    private RoleEnum role;
 }
