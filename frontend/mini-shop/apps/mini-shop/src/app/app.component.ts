d80564871cfa5cfc6d49758f1c7fb8b2d33bfb64
julitakopczynska <julita.kopczynska@lemisoft.pl>
2023-11-04 22:38:45 +0100

    Mini-shop frontend created. Dividing application for admin and mini-shop app. Libs added.

---
+++ frontend/mini-shop/apps/mini-shop/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mini-shop-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent implements OnInit {
+  title = 'mini-shop';
+
+  contructor() {}
+
+  ngOnInit() {}
+}
---
+++ frontend/mini-shop/apps/mini-shop/src/app/pages/product-list/product-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mini-shop-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.scss'],
+})
+export class ProductListComponent {}
