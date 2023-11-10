bbfb8ca18efdcfe36a9e43461d7e19ae15dbc279
kmachura-mika <katarzyna.machura-mika@opi.org.pl>
2023-11-10 21:03:27 +0100

    initial commit of the project's structure for 4 microservices: products, orders, users, deliveries

---
+++ backend/products/src/test/java/com/microservices/minishop/products/ProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.minishop.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
